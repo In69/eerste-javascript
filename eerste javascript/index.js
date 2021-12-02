cdd7dd5de3022f2cbba8d6f2d566b6ddb6c92f61
In69 <90973339+In69@users.noreply.github.com>
2021-12-02 11:52:21 +0100

    Add files via upload

---
+++ eerste javascript/index.js
@@ -0,0 +1,30 @@
+console.log("Hello Winc Academy")
+console.log( "Inge Jacobs")
+
+let firstname = "inge";
+let lastname = "Jacobs";
+let age = "52";
+console.log(typeof 52);
+
+"4" + "4" = 8;
+
+let x = 5;
+let y = 2;
+let z = x + y;
+
+let x = 5;
+let y = 2;
+let z = x - y;
+
+let x = 5;
+let y = 2;
+let z = x * y;
+
+let x = 5;
+let y = 2;
+let z = x / y;
+
+let x = 5;
+let y = 2;
+let z = x % y;
+
